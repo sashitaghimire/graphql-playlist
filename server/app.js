2421e97230979445fa97f0f08443ee37c03ba3d5
sashitaghimire <ghimiresashita@gmail.com>
2021-06-21 13:52:44 +0545

    graphql schema, root query, resolve function

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
 
 const app = express();
 
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema
+}));
+
 app.listen(4000, () => {
- console.log("now listening on port 4000")
-})
+    console.log('now listening for requests on port 4000');
+});
