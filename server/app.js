0d4729e5687c2911a3a44f009090934654384f45
sashitaghimire <ghimiresashita@gmail.com>
2021-06-30 14:01:06 +0545

    connect mLab, mutation

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to database
+
+mongoose.connect('mongodb+srv://john:john123@cluster0.s8e1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log("connected to the database")
+})
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
---
+++ server/models/books.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const bookSchema = new Schema({
+ name: String,
+ author: String,
+ genre:String,
+})
+
+module.exports = mongoose.model('Book', bookSchema);
